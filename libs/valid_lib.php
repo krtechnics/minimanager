2fd980da06d7f12fa0dd08fcba4ccf1b6ce403b9
Aokromes <Aokromes@users.noreply.github.com>
2018-12-07 19:05:05 +0100

    require php 7.0
    Still need to fix this warning:
    PHP message: PHP Warning:  mysqli_real_escape_string() expects exactly 2 parameters, 1 given in libs/valid_lib.php on line 80

--- libs/valid_lib.php
+++ libs/valid_lib.php
@@ -76,8 +76,8 @@ function cleanSQL($string)
     if(get_magic_quotes_gpc())  // prevents duplicate backslashes
         $string = stripslashes($string);
 
-    if (phpversion() >= '4.3.0')
-        $string = mysql_real_escape_string($string);
+    if (phpversion() >= '7.0')
+        $string = mysqli_real_escape_string($string);
     else
         $string = mysql_escape_string($string);
 
